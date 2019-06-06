3e118e7beb85d8d2acdf011307de542b6b0506fa
jinhak Kim <smiling.jinhak@gmail.com>
2019-06-06 23:56:12 +0900

    spring boot batch coomit

---
+++ src/main/java/com/community/batch/domain/User.java
@@ -0,0 +1,78 @@
+package com.community.batch.domain;
+
+import com.community.batch.domain.enums.Grade;
+import com.community.batch.domain.enums.SocialType;
+import com.community.batch.domain.enums.UserStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@EqualsAndHashCode(of = {"idx", "email"})
+@NoArgsConstructor
+@Entity
+@Table
+public class User implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column
+    private String name;
+
+    @Column
+    @JsonIgnore // JsonIgnore를 사용하면 해당 필드가 반환값에 포함안됨
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private String principal;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Grade grade;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @Builder
+    public User(String name, String password, String email, String principal,
+                SocialType socialType, UserStatus status, LocalDateTime createdDate,
+                LocalDateTime updatedDate){
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.principal = principal;
+        this.socialType = socialType;
+        this.status = status;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+
+    public User setInactive() {
+        status = UserStatus.INACTIVE;
+        return this;
+    }
+}
+
--- src/main/java/com/community/controller/TestController.java
+++ src/main/java/com/community/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package com.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
